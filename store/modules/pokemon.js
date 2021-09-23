d0ae47df7963c2ad45bacdf0b818575de8fe7db8
raphael <raphael@mcledger.com>
2021-09-23 17:15:12 +0200

    Add vuex

---
+++ store/modules/pokemon.js
@@ -0,0 +1,20 @@
+const state = () => ({
+	list : []
+})
+
+const getters = {
+}
+
+const mutations = {
+
+}
+const actions = {
+
+}
+export default {
+	namespaced : true,
+	state,
+	getters,
+	mutations,
+	actions
+}
