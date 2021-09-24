28dc7d46f2f614af6a067ed196a5e01082c95c8a
raphael <raphael@mcledger.com>
2021-09-24 18:17:40 +0200

    Add icon component

---
+++ components/icon.vue
@@ -0,0 +1,42 @@
+<template>
+	<div v-html="require(`!html-loader!@/static/icons/${icon.toLowerCase()}.svg`)" :class="'icon icon-' + icon" />
+</template>
+<script>
+export default {
+	name  : 'SvgIcon',
+	props : {
+		icon : {
+			type    : String,
+			default : null
+		},
+	},
+	methods : {
+		removeFillRecursivly(el) {
+			if(el.hasAttribute('fill') && el.getAttribute('fill') != 'none') {
+				el.removeAttribute('fill')
+			}
+			if(el.children.length) {
+				for (let i = 0; i < el.children.length; i++) {
+					const child = el.children[i]
+					this.removeFillRecursivly(child)
+				}
+			}
+		}
+	},
+	mounted() {
+		if (this.$el.firstElementChild.nodeName === 'svg') {
+			const svg = this.$el.firstElementChild
+			this.removeFillRecursivly(svg)
+			svg.setAttribute('height', '1.2em')
+			svg.setAttribute('width', '1.2em')
+			svg.classList.add('svg-class')
+		}
+	}
+}
+</script>
+<style scoped>
+.icon {
+	display: inline-flex;
+	fill: currentColor;
+}
+</style>
