4ff079e77325927b4ba10c74ab6f7c8eef54011b
raphael <raphael@mcledger.com>
2021-09-25 15:05:10 +0200

    Fix some details to pokemon list

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
 	<div>
+		<input type="text" @input="search($event)" placeholder="Search" id="search">
 		<div class="pokemon-list">
-			<Pokemon v-for="(pokemon, index) in pokemons" :key="index" :pokemon="pokemon" />
+			<Pokemon v-for="(pokemon, index) in pokemonsFiltered" :key="index" :pokemon="pokemon" />
 		</div>
 		<Pokedex />
 	</div>
@@ -10,13 +11,17 @@
 <script>
 import { Pokemon, Pokedex } from './list/'
 export default {
+	head : {
+		title : 'Pokédex'
+	},
 	components : {
 		Pokemon,
 		Pokedex
 	},
 	data() {
 		return {
-			pokemons : []
+			pokemons         : [],
+			pokemonsFiltered : []
 		}
 	},
 	async created() {
@@ -26,12 +31,23 @@ export default {
 		loadPokemons() {
 			this.$axios.$get('https://pokeapi.co/api/v2/pokemon?limit=151').then(res => {
 				this.pokemons = res.results
+				this.pokemonsFiltered = res.results
+			})
+		},
+		search($event) {
+			this.pokemonsFiltered = this.pokemons.filter((x) => {
+				return x.name.includes($event.target.value.toLowerCase())
 			})
 		}
 	}
 }
 </script>
 <style lang="scss" scoped>
+#search {
+	position: relative;
+	left: 10px;
+	margin: 10px 0;
+}
 .pokemon-list {
 	display: grid;
 	grid-template-areas: 'a b c d';
--- pages/pokemon.vue
+++ pages/pokemon.vue
@@ -1,5 +1,72 @@
 <template>
 	<div id="pokemon">
-		Test
+		<spinner v-if="loading" variant="blue" size="xl" />
+		<div v-else>
+			<div class="d-flex justify-content-between align-items-center">
+				<div class="d-flex align-items-center">
+					<h1>{{ pokemon.name }}</h1>
+					<img :src="pokemon.sprites.front_default">
+				</div>
+				<button class="blue" @click="$router.go(-1)">
+					Return
+				</button>
+			</div>
+			<div>
+				Types :
+				<span v-for="(type, i) in pokemon.types" :key="i">
+					{{ type.type.name }}
+				</span>
+			</div>
+			<div>
+				Abilites :
+				<span v-for="(abilitie, i) in pokemon.abilities" :key="i">
+					{{ abilitie.ability.name }}
+				</span>
+			</div>
+			<div>
+				Statistic :
+				<ul>
+					<li v-for="(stat, i) in pokemon.stats" :key="i">
+						{{ stat.base_stat }} {{ stat.stat.name }}
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </template>
+<script>
+export default {
+	head : {
+		title : 'Pokémon'
+	},
+	data() {
+		return {
+			pokemon : null,
+			loading : true
+		}
+	},
+	async created() {
+		const name = this.$route.query.name
+		await this.$axios.$get(`https://pokeapi.co/api/v2/pokemon/${name}`).then(res => {
+			this.pokemon = res
+			console.log(res)
+			this.loading = false
+		})
+	}
+}
+</script>
+<style lang="scss" scoped>
+#pokemon {
+	padding: 0 10px;
+}
+h1 {
+	text-transform: capitalize;
+	margin: 0;
+}
+.spinner {
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+}
+</style>
