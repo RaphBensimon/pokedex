51aca06e451ea00ca83d5373c3a9b53256c10c24
raphael <raphael@mcledger.com>
2021-09-25 15:03:23 +0200

    Add logo

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="app">
+		<img src="@/static/logo.svg">
+		<Nuxt />
+	</div>
+</template>
+<style lang="scss" scoped>
+img {
+	position: relative;
+	left: 50%;
+	transform: translateX(-50%);
+	width: 90%;
+	max-width: 300px;
+}
+</style>
